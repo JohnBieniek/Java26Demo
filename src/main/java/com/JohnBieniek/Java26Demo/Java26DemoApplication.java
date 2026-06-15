1fd60b2a86b82c7447590ed223532265a99eeb76
John Doe <johnBieniekGT@gmail.com>
2026-06-15 18:02:49 -0400

    Initial commit

---
+++ src/main/java/com/JohnBieniek/Java26Demo/Java26DemoApplication.java
@@ -0,0 +1,13 @@
+package com.JohnBieniek.Java26Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java26DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java26DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JohnBieniek/Java26Demo/Java26DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JohnBieniek.Java26Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java26DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
